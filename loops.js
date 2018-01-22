e68ebfb45a2629ee32a19695326748a3b4b71006
Eli Cleveland <elicleveland12@gmail.com>
2018-01-22 21:19:30 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,24 @@
+/* for ([initialization]; [condition]; [iteration]) {
+  [loopBody];
+} 
+
+i is set equal to 1 
+as long as i is less than 100, execute the code in the loopBody
+which is print "Hello World"; increment i each time the code in loopBody is executed
+
+for (var i = 1; i < 100; i++) {
+  console.log("Hello World the " + i + " time" );
+}*/
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i = 1) {
+      console.log("I am ${i} strange loop.");
+    }
+    else {
+      console.log("I am ${i} strange loops.");
+    }
+  }
+}
+
+
